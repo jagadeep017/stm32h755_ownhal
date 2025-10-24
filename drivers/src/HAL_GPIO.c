1afc597fea679dc706ceb33a2762c626907f15db
Jagadeep Ganta <152236029+jagadeep017@users.noreply.github.com>
2025-10-24 19:04:23 +0530

    Remove IRQ configuration and handler functions
    Removed IRQ configuration and handler functions from HAL_GPIO.c.
    and defined a generic IRQ configuration and priority handler

--- drivers/src/HAL_GPIO.c
+++ drivers/src/HAL_GPIO.c
@@ -265,28 +265,6 @@ void HAL_GPIO_toggle_pin(GPIO_reg_t *GPIOx, uint8_t pin_num){
 	GPIOx->ODR ^= (1<<pin_num);
 }
 
-/*
- * IRQ configuration and handler
- */
-void HAL_GPIO_irq_config(uint8_t irq_number, uint8_t type){
-	//TODO need to check if correct parameters were passed
-	uint8_t ind = irq_number/32			//index of ISER or ICER
-			,pos = irq_number%32;		//position of that bit in ISER or ICER
-	if(type == ENABLE){
-		NVIC_ISER[ind] |= (1<<pos);
-	}
-	else{
-		NVIC_ICER[ind] |= (1<<pos);
-	}
-}
-
-
-void HAL_GPIO_irq_priority(uint8_t irq_number, uint8_t priority){
-	uint8_t ind = irq_number/4			//index of the register in IPR
-			,pos = irq_number%4;		//position of bits in IPR
-	//setting the priority
-	NVIC_IPR[ind] |= ((uint32_t)priority<<(8*pos+4)); 		//because only 4 msb's are implemented in stm32 IPR
-}
 
 void HAL_GPIO_irq_handler(uint8_t pin_num){
 	if(EXTI->C1PR1 & (1<<pin_num)){
